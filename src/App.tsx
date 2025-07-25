d1ecc18a03de91ca4f2fd6b25c89c2812550d907
Mikhail <mikthefirst11@gmail.com>
2025-07-25 18:05:54 +0300

    ready component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import TableCrud from './components/Table/TableCrud';
+import 'antd/dist/reset.css'; 
+
+
+function App() {
+  return (
+    <div className="App">
+      <TableCrud />
+    </div>
+  );
+}
+
+export default App;
